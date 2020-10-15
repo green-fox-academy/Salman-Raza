abdf303a6ae20a186afbd3f035ed3a9bdb606e48
Salman791 <72814767+Salman791@users.noreply.github.com>
2020-10-15 14:27:40 +0200

    Initialize dependabot/npm_and_yarn/websocket-extensions-0.1.4

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '/output/coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 25,
+        lines: 25,
+        branches: 25,
+        functions: 25
+      }
+    },
+    junitReporter    : {
+      outputDir : 'output/test-results',
+      outputFile : 'test-results.xml'
+    },
+    reporters: ['progress', 'junit'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: false
+  });
+};
